a471304d240ae175233484010e5d8d39d8f798c4
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2018-10-14 16:36:32 +0530

    combined all diamond generating programs into one

--- print_diamonds.js
+++ print_diamonds.js
@@ -1,27 +1,105 @@
-let diamondType = process.argv[2];
-let height = process.argv[3];
-
-const generateLine = function(symbol, length){
-  let line = "";
-  for(let index = 0; index < length; index++){
-    line += symbol;
+const fillWithStar = function(heightOfDiamond){
+  let filledString = "";
+  for(let index = 0; index < heightOfDiamond; index++){
+    filledString += "*";
   }
-  return line;
+  return filledString;
 }
 
-const generateDiamond = function(height){
-  let diamond = "";
-  for(let row = 0;row < height;row++){
-    let lineLength = row + 1;
-    if(row >= height/2){
-      lineLength = height - lineLength;
-    }
-    diamond += generateLine(" ", height/2 - lineLength);
-    diamond += generateLine("*", lineLength);
-    diamond += generateLine("*", lineLength -1) + "\n";
+const fillWithSpace = function(heightOfDiamond){
+  let filledString = "";
+  for(let index = 0; index < heightOfDiamond; index++){
+    filledString += " ";
   }
-  
+  return filledString;
+}
+
+const fillSpaceEndWithStar = function(heightOfDiamond){
+  let filledString = "";
+  for(let index =0; index < heightOfDiamond; index++){
+    filledString += " ";
+  }
+  return filledString + "*";
+}
+
+const filledDiamond = function(heightOfDiamond){
+  let diamond = ""; let unwantedStars = 1;
+  for(let row = 0; row < heightOfDiamond-1; row++){
+    numOfSpacesNeeded = Math.abs((heightOfDiamond - unwantedStars)/2);
+    diamond += fillWithSpace(numOfSpacesNeeded);
+
+    numOfStarNeeded = (heightOfDiamond - (numOfSpacesNeeded*2));
+    diamond += fillWithStar(numOfStarNeeded) + "\n";
+    unwantedStars += 2;
+  }
+  numOfSpacesNeeded = Math.abs((heightOfDiamond - unwantedStars)/2);
+  diamond += fillSpaceEndWithStar(numOfSpacesNeeded);
   return diamond;
 }
 
-console.log(generateDiamond(height));
+const hollowDiamond = function(heightOfDiamond){
+  let unwantedStars = 3; let diamond = "";
+  numOfSpacesNeeded = Math.abs((heightOfDiamond - unwantedStars)/2 +1);
+  diamond += fillSpaceEndWithStar(numOfSpacesNeeded) + "\n";
+
+  for(let row = 1; row < heightOfDiamond -1; row++){
+    numOfSpacesNeeded = Math.abs((heightOfDiamond - unwantedStars)/2);
+    diamond += fillSpaceEndWithStar(numOfSpacesNeeded);
+
+    numOfStarNeeded = (heightOfDiamond - (numOfSpacesNeeded*2));
+    diamond += fillSpaceEndWithStar(Math.abs(numOfStarNeeded - 2)) + "\n";
+    unwantedStars += 2;
+  }
+
+  numOfSpacesNeeded = Math.abs((heightOfDiamond - unwantedStars)/2);
+  diamond += fillSpaceEndWithStar(numOfSpacesNeeded);
+
+  return diamond;
+}
+
+const angledDiamond = function(heightOfDiamond){
+  let diamond = "";
+  let unwantedStars = 3;
+  let leftSideSymbol = "/"; 
+  let rightSideSymbol = "\\";
+  numOfSpacesNeeded = Math.abs((heightOfDiamond - unwantedStars)/2 + 1 );
+  diamond += fillSpaceEndWithStar(numOfSpacesNeeded) + "\n";
+
+  for(let row = 1; row < heightOfDiamond - 1; row++){
+    numOfSpacesNeeded = Math.abs((heightOfDiamond - unwantedStars)/2);
+    diamond += fillWithSpace(numOfSpacesNeeded);
+    if(numOfSpacesNeeded == 0){
+      diamond += "*" + fillWithSpace(heightOfDiamond -2)+ "*"+"\n";
+      leftSideSymbol = "\\";
+      rightSideSymbol = "/";
+      unwantedStars += 2;
+      continue;
+    }
+    numOfStarNeeded = (heightOfDiamond - (numOfSpacesNeeded*2));
+    diamond += leftSideSymbol+fillWithSpace(numOfStarNeeded -2) + rightSideSymbol+"\n";
+    unwantedStars += 2;
+  }
+  numOfSpacesNeeded = Math.abs((heightOfDiamond - unwantedStars)/2);
+  diamond += fillSpaceEndWithStar(numOfSpacesNeeded);
+  return diamond;
+}
+
+const main = function(){
+  let type = process.argv[2];
+  let heightOfDiamond = +process.argv[3];
+  let diamond;
+
+  if(type == "simple"){
+    diamond = filledDiamond(heightOfDiamond);
+  }
+  if(type == "hollow"){
+    diamond = hollowDiamond(heightOfDiamond);
+  }
+  if(type == "angled"){
+    diamond = angledDiamond(heightOfDiamond);
+  }
+
+  console.log(diamond);
+}
+
+main();
